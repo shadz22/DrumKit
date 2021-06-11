76275fe5a4dda96fd93aaa9930f1c8f56578c6b5
shadz22 <shadz22@yahoo.co.uk>
2021-06-11 18:11:06 +0100

    Add effects to the keys when they get played

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 window.addEventListener('keydown', function(e) {
-  console.log(e);
-  const audio = document.querySelector(`audio[data-key=${e.code}]`)
+  const audio = document.querySelector(`audio[data-key=${e.code}]`);
+  const key = document.querySelector(`.key[data-key=${e.code}]`);
   if (!audio) return;
+  key.classList.add('playing');
   audio.currentTime = 0;
   audio.play();
-  
-})
+});
+
