ab1986b6c70f48c9aadccbcb6e4c683b36966944
shadz22 <shadz22@yahoo.co.uk>
2021-06-14 11:26:25 +0100

    Refactoring - create a separate function for the 'keydown' event

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-
-window.addEventListener('keydown', function(e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key=${e.code}]`);
   const key = document.querySelector(`.key[data-key=${e.code}]`);
   if (!audio) return;
   key.classList.add('playing');
   audio.currentTime = 0;
   audio.play();
-});
+}
 
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
   this.classList.remove('playing');
-  
 }
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
